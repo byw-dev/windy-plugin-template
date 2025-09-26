74d5aae5a8c074b2c3525e9d9d67e181e6095e30
tsingye <me@tsingye.com>
2025-09-27 02:00:57 +0800

    更新了插件描述中的代码仓库URL

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -7,7 +7,7 @@ const config: ExternalPluginConfig = {
     title: 'UAV WMOCS',
     description: 'Used to display WMOCS data for UAV operations',
     author: 'Byweather (optional company name)',
-    repository: 'https://github.com/windycom/windy-plugin-template',
+    repository: 'https://github.com/byw-dev/windy-plugin-template',
     desktopUI: 'rhpane',
     mobileUI: 'fullscreen',
     routerPath: '/uav-wmocs',
